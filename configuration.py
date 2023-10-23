a13afad718610dd2e535dc2f568ee4f7f9017d45
dmitry.itigin <dmitry.itigin@gmail.com>
2023-10-24 00:40:13 +0600

    My frst commit!

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVER = 'https://627b9afb-cbd3-4a55-aa47-4dba1b19a39d.serverhub.praktikum-services.ru' # Адрес стенда
+CREATE_ORDERS = '/api/v1/orders' # Создание заказа
+GET_ORDERS = '/v1/orders/track' # Получение трека заказа
